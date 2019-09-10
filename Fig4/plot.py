6a011220dcae4ff171fca7bc25031c5138c54ce8
luigi sbailo <luigi.sbailo@gmail.com>
2019-09-10 17:50:40 +0200

    new color

--- Fig4/plot.py
+++ Fig4/plot.py
@@ -13,12 +13,12 @@ data_annih = np.loadtxt('plot_annih_proj.out')
 marker_size=6
 x = np.arange(0,1000,1)
 ax1.plot (x, 6*0.01*x,  '--', color='grey', linewidth=1.)
-ax1.plot (data_diff[:,0], data_diff[:,1], 'v', color='#336699', markersize=marker_size, label = r'$g_{\Omega}(r,t|0,0)$')
+ax1.plot (data_diff[:,0], data_diff[:,1], 'v', color='#446280', markersize=marker_size, label = r'$g_{\Omega}(r,t|0,0)$')
 ax1.plot (data_diff[:,0], data_diff[:,2], '^', color='#CC6633', markersize=marker_size, label = r'$g_{\Omega}(r,t|0,0;\tau)$')
 ax1.plot (data_diff[:,0], data_diff[:,3],  'x', color='#446280', markersize=4, label = r'$BD$')
 
 x_arr = np.array([3,7,11,15,19])
-ax2.plot (data_annih[x_arr,0], data_annih[x_arr,1], 'v', color='#336699',markersize=marker_size, label = r'$g_{\Omega}(r,t|0,0)$')
+ax2.plot (data_annih[x_arr,0], data_annih[x_arr,1], 'v', color='#446280',markersize=marker_size, label = r'$g_{\Omega}(r,t|0,0)$')
 ax2.plot (data_annih[x_arr,0], data_annih[x_arr,2], '^', color='#CC6633', markersize=marker_size,label = r'$g_{\Omega}(r,t|0,0;\tau)$')
 ax2.plot (data_annih[x_arr,0], data_annih[x_arr,3],  'x', color='#446280', markersize=4, label = r'$BD$')
 
